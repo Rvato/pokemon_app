77540ce298b390c26a4ed40eb48118458194e2ac
YuuKi <6_rubato-myuism_2907@ezweb.ne.jp>
2020-08-25 10:18:06 +0900

    データベースの修正

--- db/migrate/20200817103152_create_markings.rb
+++ db/migrate/20200817103152_create_markings.rb
@@ -1,7 +1,7 @@
 class CreateMarkings < ActiveRecord::Migration[5.2]
   def change
     create_table :markings do |t|
-      t.references    :pokemon, null: false, foreign_key: true
+      t.references    :pokemon, foreign_key: true
       t.timestamps
     end
   end
