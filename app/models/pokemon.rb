416371bb00959df2c0a52a3926a55d9d212e651a
YuuKi <6_rubato-myuism_2907@ezweb.ne.jp>
2020-09-11 00:19:58 +0900

    f.fields_forの記述

--- app/models/pokemon.rb
+++ app/models/pokemon.rb
@@ -1,10 +1,3 @@
 class Pokemon < ApplicationRecord
-  has_one    :current_status
-  has_one    :base_status
-  has_one    :effort_value
   belongs_to :party
-  
-  accepts_nested_attributes_for :current_status
-  accepts_nested_attributes_for :base_status
-  accepts_nested_attributes_for :effort_value
 end
