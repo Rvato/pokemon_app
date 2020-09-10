e5eeb84717ec114b19a4cccb431861fd3ac22b5b
YuuKi <6_rubato-myuism_2907@ezweb.ne.jp>
2020-09-10 16:04:26 +0900

    ルーティングの修正

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   root to: 'tops#index'
 
-  resources :parties, only: [:index, :new, :create, :edit, :update, :destroy] do
+  resources :parties, only: [:index, :new, :create, :show, :edit, :update, :destroy] do
     resources :pokemons, only: [:new, :create]
   end
 
