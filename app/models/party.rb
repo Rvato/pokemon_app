a768a7d81a6672a646d65be622a864019c8d909d
YuuKi <6_rubato-myuism_2907@ezweb.ne.jp>
2020-09-12 10:12:55 +0900

    モデルの修正

--- app/models/party.rb
+++ app/models/party.rb
@@ -1,5 +1,5 @@
 class Party < ApplicationRecord
-  has_many  :pokemons
+  has_many                      :pokemons
   accepts_nested_attributes_for :pokemons
 
   validates :party_name, presence: true
